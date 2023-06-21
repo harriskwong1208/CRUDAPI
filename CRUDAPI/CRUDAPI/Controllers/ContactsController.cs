2993e6abbc10b3b7e2005137f69013a44b10a6d0
Harris Kwong <harriskwong120898@gmail.com>
2023-06-21 14:56:48 -0400

    Set up contact model and contactscontroller

---
+++ CRUDAPI/CRUDAPI/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using CRUDAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        //Use dbContext to talk to in memory database
+        private readonly CRUDAPIDbContext dbContext;
+
+        public ContactsController(CRUDAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());
+           
+        }
+    }
+}
---
+++ CRUDAPI/CRUDAPI/Data/CRUDAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPI.Data
+{
+    public class CRUDAPIDbContext : DbContext
+    {
+        public CRUDAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
